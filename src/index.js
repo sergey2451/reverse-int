9520c3ea635eeaf8f6676d8c6a45ad7bc96d8423
sergey2451 <kragel.s@mail.ru>
2022-02-03 12:06:12 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  	let newStr = '';
+	let str = String(Math.abs(n));
+
+	for (let i = str.length-1; i >= 0; i--) {
+		newStr += str[i]
+	}
+	return +newStr;
 }
